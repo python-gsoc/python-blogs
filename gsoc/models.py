88a8e63d8a29a05257049a11f569f6fe43486042
Shashank Sharma <shashank.sharma98@gmail.com>
2019-02-12 02:51:09 +0530

    suborg + gsoc year models added with blog_meta

--- gsoc/models.py
+++ gsoc/models.py
@@ -1,9 +1,37 @@
-#from django.contrib.auth.models import AbstractUser
-#from django.db import models
+from django.contrib import auth
+from django.contrib.auth.models import User
+from django.db import models
 
-#class User(AbstractUser):
-#    name = models.CharField(max_length=100, blank=True, null=True)
-#    sites = models.ManyToManyField('sites.Site')
 
-#    def get_sites(self):
-#        return self.sites
+class SubOrg(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    suborg_full_name = models.CharField(name='suborg_full_name', max_length=80)
+
+    def __str__(self):
+        return self.suborg_full_name
+
+
+class GsocYear(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    gsoc_year = models.CharField(name='gsoc_year', max_length=5)
+
+    def __str__(self):
+        return self.gsoc_year
+
+
+def suborg_full_name(self):
+    try:
+        return SubOrg.objects.get(user=self.id)
+    except SubOrg.DoesNotExist:
+        return None
+
+
+def gsoc_year(self):
+    try:
+        return GsocYear.objects.get(user=self.id)
+    except GsocYear.DoesNotExist:
+        return None
+
+
+auth.models.User.add_to_class('suborg_full_name', suborg_full_name)
+auth.models.User.add_to_class('gsoc_year', gsoc_year)
