4af3ffad64b910594d639f0f3f6e036376a9937c
botanicvelious <cyberempires@gmail.com>
2019-02-17 12:44:17 -0700

    change build db script

---
+++ builddb.sh
@@ -0,0 +1,3 @@
+python manage.py migrate --database default 
+python manage.py migrate --database auth_db 
+python manage.py createsuperuser --database auth_db
--- do.sh
+++
@@ -1,5 +0,0 @@
-python manage.py migrate --database default 
-python manage.py migrate --database auth_db auth
-python manage.py migrate --database auth_db admin
-python manage.py migrate --database auth_db sessions
-python manage.py createsuperuser --database auth_db
