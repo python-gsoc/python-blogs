b1eb5df2e2a5cf58b7fabe9a1da9c3f57dc79d06
botanicvelious <cyberempires@gmail.com>
2019-02-18 06:07:31 -0700

    add scripts folder

---
+++ scripts/builddb.bat
@@ -0,0 +1,5 @@
+del project.db
+del users.db
+python manage.py migrate --database default 
+python manage.py migrate --database auth_db 
+python manage.py createsuperuser --database auth_db
--- scripts/builddb.sh
+++ scripts/builddb.sh
@@ -1,3 +1,5 @@
+rm project.db
+rm users.db
 python manage.py migrate --database default 
 python manage.py migrate --database auth_db 
 python manage.py createsuperuser --database auth_db
