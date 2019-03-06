16d833d89079c0e99bcad7766cf4df0a3d5d6a9b
ntkomata <ntkomata@outlook.com>
2019-03-06 12:09:42 +0800

    fix userprofile form

--- gsoc/forms.py
+++ gsoc/forms.py
@@ -3,7 +3,7 @@ from .models import UserProfile
 
 class UserProfileForm(ModelForm):
     class Meta:
-        fields = ('role', 'suborg_full_name', 'gsoc_year', 'is_student', 'accepted_proposal_pdf')
+        fields = ('role', 'suborg_full_name', 'gsoc_year', 'accepted_proposal_pdf')
         model = UserProfile
 
 class ProposalUploadForm(ModelForm):
