9e0427113dcc62ea76ad597d12c1e4b90789f520
ntkomata <ntkomata@outlook.com>
2019-03-06 12:00:49 +0800

    pull from upstream; fix requirements.txt

--- gsoc/forms.py
+++ gsoc/forms.py
@@ -3,5 +3,10 @@ from .models import UserProfile
 
 class UserProfileForm(ModelForm):
     class Meta:
-        fields = ('role', 'suborg_full_name', 'gsoc_year')
+        fields = ('role', 'suborg_full_name', 'gsoc_year', 'is_student', 'accepted_proposal_pdf')
         model = UserProfile
+
+class ProposalUploadForm(ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['accepted_proposal_pdf']
--- gsoc/models.py
+++ gsoc/models.py
@@ -47,3 +47,40 @@ class UserProfile(models.Model):
     role = models.IntegerField(name='role', choices=ROLES, default=0)
     gsoc_year = models.ForeignKey(GsocYear, on_delete=models.CASCADE, null=True, blank=False)
     suborg_full_name = models.ForeignKey(SubOrg, on_delete=models.CASCADE, null=True, blank=False)
+    is_student = models.BooleanField(default=True)
+    accepted_proposal_pdf = models.FileField(blank=True, null=True)
+
+
+# Auto Delete Redundant Proposal
+@receiver(models.signals.post_delete, sender=UserProfile)
+def auto_delete_proposal_on_delete(sender, instance, **kwargs):
+    """
+    Deletes proposal after a UserProfile object is deleted.
+    """
+    if instance.accepted_proposal_pdf:
+        try:
+            filepath = instance.accepted_proposal_pdf.path
+        except ValueError:
+            return
+        if os.path.isfile(filepath):
+            os.remove(filepath)
+
+
+@receiver(models.signals.pre_save, sender=UserProfile)
+def auto_delete_proposal_on_change(sender, instance, **kwargs):
+    """
+    Deletes old proposal before a new one is uploaded.
+    """
+    if not instance.pk:
+        return
+    try:
+        old_file = UserProfile.objects.get(pk=instance.pk).accepted_proposal_pdf
+        old_file_path = old_file.path
+    except UserProfile.DoesNotExist:
+        return
+    except ValueError:
+        return
+    new_file = instance.accepted_proposal_pdf
+    if not old_file == new_file:
+        if os.path.isfile(old_file_path):
+            os.remove(old_file_path)
