72add52508e00eb9247b78b65e521e41538898ee
SG0304986 <abhinav.s@sabre.com>
2019-03-09 02:10:33 +0530

    Theme for student-blog

--- gsoc/urls.py
+++ gsoc/urls.py
@@ -11,6 +11,7 @@ from django.contrib.sitemaps.views import sitemap
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.static import serve
 from django.urls import path
+from . import views
 
 admin.autodiscover()
 
@@ -29,7 +30,9 @@ urlpatterns += [
 
 urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
-    url(r'^', include('cms.urls')),
+    path(r'student-blogs/', views.blog),
+    url(r'student-blogs/([-\w]+)/$', views.article, name="article"),
+    url(r'^', include('cms.urls'))
 )
 # This is only needed when using runserver.
 if settings.DEBUG:
