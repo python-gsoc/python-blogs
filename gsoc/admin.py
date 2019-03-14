a58e3a7b7e3edca5e9dd1553629fa903412e8051
ntkomata <ntkomata@outlook.com>
2019-03-14 15:36:00 +0800

    change article admin get form method

--- gsoc/admin.py
+++ gsoc/admin.py
@@ -5,7 +5,6 @@ from .models import UserProfile
 from .forms import UserProfileForm
 from aldryn_newsblog.admin import ArticleAdmin
 from aldryn_newsblog.models import Article
-from aldryn_people.models import Person
 
 class UserProfileInline(admin.TabularInline):
     model = UserProfile
@@ -20,42 +19,36 @@ admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 
 
-"""
-Make some admin-only fields not editable for students,
-and give some fields initial value.
-"""
-
-def article_get_form(self, request, obj=None, **kwargs):
-    """
-    Gives some initial form value for students.
-    """
-    is_request_by_student = request.user.student_profile() is not None
-    form = super(ArticleAdmin, self).get_form(request, obj, **kwargs)
-    if is_request_by_student is False:
-        return form
-    try:
-        default_author = Person.objects.get(user=request.user)
-    except Person.DoesNotExist:
-        default_author = None
-    if default_author is not None:
-        form.base_fields['related'].initial = default_author
-    return form
-def article_get_readonly_fields(self, request, obj=None):
+def article_get_form():
     """
     Makes some admin-only fields readonly for students.
     """
-    student_readonly_fields = [
-        'author',
-        'publishing_date',
-        'is_featured',
-        'featured_image',
-    ]
-    is_request_by_student = request.user.student_profile() is not None
-    if not is_request_by_student:
-        return self.readonly_fields
-    else:
-        return student_readonly_fields + list(self.readonly_fields)
-# ArticleAdmin.get_form = article_get_form
-ArticleAdmin.get_readonly_fields = article_get_readonly_fields
+    ori_get_form = ArticleAdmin.get_form
+    ori_readonly_fields = None
+    ori_field_sets = None
+    def return_func(self, request, obj=None, **kwargs):
+        nonlocal ori_readonly_fields, ori_field_sets
+        is_request_by_student = request.user.student_profile() is not None
+        if ori_readonly_fields is None:
+            ori_readonly_fields = self.readonly_fields
+        if ori_field_sets is None:
+            ori_field_sets = self.__class__.fieldsets
+        self.readonly_fields = ori_readonly_fields
+        self.__class__.field_sets = ori_field_sets
+        if is_request_by_student:
+            self.__class__.field_sets = self.__class__.field_sets[0]
+            print(self.__class__.field_sets)
+        form = ori_get_form(self, request, obj, **kwargs)
+        if is_request_by_student:
+            self.readonly_fields = [
+                'author',
+                'publishing_date',
+                'is_featured',
+                'featured_image',
+            ]
+        return form
+    return return_func
+ArticleAdmin.get_form = article_get_form()
+
 admin.site.unregister(Article)
 admin.site.register(Article, ArticleAdmin)
