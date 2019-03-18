25b95edd88984353f6408ec20f80d196e35de675
Debajyoti Dutta <debajyotidofficial@gmail.com>
2019-03-18 21:22:33 +0530

    Added code for to check article is 7 days old or not.

--- gsoc/common/utils/autoemail.py
+++ gsoc/common/utils/autoemail.py
@@ -2,20 +2,24 @@ from django.db import models
 from gsoc.models import UserProfile
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
+from aldryn_newsblog.models import Article
 
 #Really horrible code ahead. Be careful.
 #Need to add whether the post is 7 days old or not.
 
 def SentMail():
     email_list_count=len(UserProfile.objects.all())
-
+    
     for i in range(1,email_list_count+1):
         user = User.objects.get(id=i)
         user_email = user.email
-        send_mail(
-        'It\'s been 7 days',
-        'Post something already.',
-        'from@example.com',
-        [user_email],
-        fail_silently=False,
-    )
+        user_name= user.get_username()
+        article=Article.objects.filter(author=user_name)
+        if article.publishing_date > 7:
+            send_mail(
+            'It\'s been 7 days',
+            'Post something already.',
+            'from@example.com',
+            [user_email],
+            fail_silently=False,
+        )
