0f5e9a24f420626d7593e67ff36f06b4bd9631e8
Debajyoti Dutta <debajyotidofficial@gmail.com>
2019-03-18 22:01:51 +0530

    Added role in automail.py

--- gsoc/common/utils/autoemail.py
+++ gsoc/common/utils/autoemail.py
@@ -2,6 +2,7 @@ from django.db import models
 from gsoc.models import UserProfile
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
+from gsoc.models import UserProfile
 from aldryn_newsblog.models import Article
 
 #Really horrible code ahead. Be careful.
@@ -12,14 +13,15 @@ def SentMail():
     
     for i in range(1,email_list_count+1):
         user = User.objects.get(id=i)
-        user_email = user.email
         user_name= user.get_username()
-        article=Article.objects.filter(author=user_name)
-        if article.publishing_date > 7:
+        user_role=UserProfile.objects.role 
+        article=list(Article.objects.filter(author=user_name))
+        article=article[::-1]
+        if (user_role == 3) and (article[0].publishing_date > 7):
             send_mail(
             'It\'s been 7 days',
             'Post something already.',
             'from@example.com',
-            [user_email],
+            [user.email],
             fail_silently=False,
         )
