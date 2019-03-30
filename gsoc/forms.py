797843b4be90f64d0f988cb262ad874c772b26de
Sounak Pradhan <sounak.98@gmail.com>
2019-03-31 01:10:37 +0530

    Stop auto-redirection when selecting section in UserProfile form

--- gsoc/forms.py
+++ gsoc/forms.py
@@ -1,12 +1,16 @@
 from .models import UserProfile, UserDetails
 
-from django.forms import ModelForm, CheckboxSelectMultiple
+from django.forms import ModelForm, CheckboxSelectMultiple, Select
 
 
 class UserProfileForm(ModelForm):
     class Meta:
         model = UserProfile
         fields = ('role', 'suborg_full_name', 'gsoc_year', 'accepted_proposal_pdf', 'app_config')
+        widgets = {
+            'app_config': Select(),
+        }
+
 
 
 class ProposalUploadForm(ModelForm):
