848b3c5b6910cdac3787778e10cc04e07bc75bcb
Sounak Pradhan <sounak.98@gmail.com>
2019-04-06 17:50:36 +0530

    Add blogs_list app and necessary styling

---
+++ blogs_list/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.http import Http404
+
+from gsoc.models import (
+    GsocYear,
+    UserProfile
+)
+from cms.models import Page
+
+def list_blogs(request):
+    gsoc_years = GsocYear.objects.all()
+    
+    blogsets = {}
+    all_years = []
+    for year in gsoc_years:
+        profiles = UserProfile.objects.filter(gsoc_year=year)
+        flag = False    
+
+        blogset = []
+        for profile in profiles:
+            if profile.app_config:
+                flag = True
+                ns = profile.app_config.namespace
+                page = Page.objects.filter(application_namespace=ns).filter(publisher_is_draft=False).first()
+                url = page.get_absolute_url()
+                student_name = profile.user.get_full_name()
+                student_username = profile.user.username
+
+                blogset.append({
+                    'title': profile.app_config.app_title,
+                    'url': url,
+                    'student': student_name if student_name else student_username,
+                    'suborg': profile.suborg_full_name.suborg_name
+                })
+        
+        if flag:
+            all_years.append(year)
+            blogsets.append(blogset)
+
+    return render(request, 'list_view.html', {
+        'years': all_years,
+        'blogsets': all_blogs
+    })
