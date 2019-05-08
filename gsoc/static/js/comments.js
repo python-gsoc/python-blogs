7c84774b3f7cff601be21793a402cd1a4809d549
Sounak Pradhan <sounak.98@gmail.com>
2019-05-08 15:30:00 +0530

    Change share button text when link copied

--- gsoc/static/js/comments.js
+++ gsoc/static/js/comments.js
@@ -1,3 +1,7 @@
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
 function showCommentForm(commentPk) {
     var form = document.getElementById('form-' + commentPk);
     form.style.display = 'block';
@@ -7,7 +11,6 @@ function showCommentForm(commentPk) {
 function copyCommentUrl(commentPk) {
     var href = window.location.href.split('#')[0]
     var str = href + '#comment-' + commentPk;
-    console.log(str);
     var el = document.createElement('textarea');
     el.value = str;
     el.setAttribute('readonly', '');
@@ -16,13 +19,18 @@ function copyCommentUrl(commentPk) {
     el.select();
     document.execCommand('copy');
     document.body.removeChild(el);
+
+    var share = document.getElementById(`share-${commentPk}`);
+    share.innerHTML = "<span>Link copied!</span>"
+    sleep(3000).then(() => {
+        share.innerHTML = `<span onclick="copyCommentUrl(${commentPk})" id="share-${commentPk}">Share</span>`;
+    })
 }
 
 (function markSelected() {
     var parts = window.location.href.split('#')
     if (parts.length == 2 && parts[1].split('-')[0] == 'comment') {
         var comment = document.getElementById(parts[1]);
-        console.log(comment);
         if (comment) {
             comment.classList.add('selected')
         }
