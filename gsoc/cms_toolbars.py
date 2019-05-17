08f76ee1c32eee08a614b74a2ccb9bd187e2da5e
Sounak Pradhan <sounak.98@gmail.com>
2019-05-17 19:17:44 +0530

    Fix pep8 warnings

--- gsoc/cms_toolbars.py
+++ gsoc/cms_toolbars.py
@@ -47,7 +47,8 @@ def add_admin_menu(self):
 
         # scheduler
         if user and user.is_superuser:
-            self._admin_menu.add_sideframe_item(_('Schedulers'), url=admin_reverse('gsoc_scheduler_changelist'))
+            self._admin_menu.add_sideframe_item(_('Schedulers'),
+                                                url=admin_reverse('gsoc_scheduler_changelist'))
         self._admin_menu.add_break(ADMINISTRATION_BREAK)
 
         # cms users settings
