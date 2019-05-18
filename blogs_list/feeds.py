9d84a6446dbd6f69344301e256e797bbcf799b8b
Sounak Pradhan <sounak.98@gmail.com>
2019-05-18 09:03:39 +0530

    Fetch content in feeds

--- blogs_list/feeds.py
+++ blogs_list/feeds.py
@@ -1,26 +1,77 @@
 from django.contrib.syndication.views import Feed
+from django.utils.feedgenerator import DefaultFeed
+from django.conf import settings
+from django.contrib.auth.models import AnonymousUser
+from django.test.client import RequestFactory
+from django.template import RequestContext
 
 from gsoc.models import UserProfile, GsocYear
 
 from aldryn_newsblog.cms_appconfig import NewsBlogConfig
 
-from cms.models import Page
+from cms.models import Page, Site
+from cms.plugin_rendering import ContentRenderer
+
+
+def get_request(language=None):
+    request_factory = RequestFactory()
+    request = request_factory.get('/')
+    request.session = {}
+    request.LANGUAGE_CODE = language or settings.LANGUAGE_CODE
+
+    # Needed for plugin rendering.
+    request.current_page = None
+    request.user = AnonymousUser()
+    return request
+
+
+class CorrectMimeTypeFeed(DefaultFeed):
+    content_type = 'application/xml; charset=utf-8'
 
 
 class BlogsFeed(Feed):
     title = "GSoC@PSF Blogs"
     link = '/blogs/'
+    feed_url = '/blogs/feed/'
+    feed_type = CorrectMimeTypeFeed
     description = 'Updates on different student blogs of GSoC@PSF'
 
     def items(self):
         gsoc_year = GsocYear.objects.first()
         ups = UserProfile.objects.filter(role=3, gsoc_year=gsoc_year).all()
-        pks = [_.app_config.pk for _ in ups]
-        return NewsBlogConfig.objects.filter(pk__in=pks).all()
+        articles = []
+        for up in ups:
+            section = up.app_config
+            articles.extend(list(section.article_set.all()))
+        return articles
+
+    def item_author_name(self, item):
+        return item.owner.username
+
+    def item_author_email(self, item):
+        return item.owner.email
 
     def item_title(self, item):
-        return item.app_title
+        return item.title
+    
+    def item_description(self, item):
+        request = get_request()
+        c = ContentRenderer(request)
+        html = c.render_placeholder(item.content, RequestContext(request))
+        return html
+
+    def item_pubdate(self, item):
+        return item.publishing_date
+
+    def item_guid(self, item):
+        site = Site.objects.first()
+        return 'http://{}{}'.format(site.domain, self.item_link(item))
+
+    def item_guid_is_permalink(self, item):
+        return True
 
     def item_link(self, item):
-        p = Page.objects.get(application_namespace=item.namespace, publisher_is_draft=False)
-        return p.get_absolute_url()
+        section = item.app_config
+        p = Page.objects.get(application_namespace=section.namespace, publisher_is_draft=False)
+        url = '{}{}/'.format(p.get_absolute_url(), item.slug, '/')
+        return url
--- gsoc/cms_wizards.py
+++ gsoc/cms_wizards.py
@@ -1,3 +1,5 @@
+from .models import UserProfile
+
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
@@ -48,6 +50,10 @@ def __init__(self, **kwargs):
     get_published_app_configs()
 
     userprofiles = self.user.userprofile_set.all()
+
+    if self.user.is_superuser:
+        userprofiles = UserProfile.objects.all()
+
     app_config_choices = []
     for profile in userprofiles:
         app_config_choices.append((profile.app_config.pk, profile.app_config.get_app_title()))
