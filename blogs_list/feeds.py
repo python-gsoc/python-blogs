8d9d03e7f61903239885a38daec6217decce83ac
Sounak Pradhan <sounak.98@gmail.com>
2019-05-18 07:11:36 +0530

    Change queryset for blog

--- blogs_list/feeds.py
+++ blogs_list/feeds.py
@@ -1,6 +1,9 @@
 from django.contrib.syndication.views import Feed
 
+from gsoc.models import UserProfile, GsocYear
+
 from aldryn_newsblog.cms_appconfig import NewsBlogConfig
+
 from cms.models import Page
 
 
@@ -10,7 +13,10 @@ class BlogsFeed(Feed):
     description = 'Updates on different student blogs of GSoC@PSF'
 
     def items(self):
-        return NewsBlogConfig.objects.all()
+        gsoc_year = GsocYear.objects.first()
+        ups = UserProfile.objects.filter(role=3, gsoc_year=gsoc_year).all()
+        pks = [_.app_config.pk for _ in ups]
+        return NewsBlogConfig.objects.filter(pk__in=pks).all()
 
     def item_title(self, item):
         return item.app_title
