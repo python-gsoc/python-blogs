5c5c9cdbe06aedb9b40b889cc4695a9a9c1c4311
Sounak Pradhan <sounak.98@gmail.com>
2019-05-26 23:25:25 +0530

    Fix pep8 warnings

--- gsoc/migrations/0018_auto_20190526_1519.py
+++ gsoc/migrations/0018_auto_20190526_1519.py
@@ -13,16 +13,24 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AlterModelOptions(
             name='adduserlog',
-            options={'verbose_name': 'Add Users (The invites will be sent to the emails on save)', 'verbose_name_plural': 'Add Users (The invites will be sent to the emails on save)'},
+            options={'verbose_name': 'Add Users (The invites will be sent to the emails on save)',
+                     'verbose_name_plural': 'Add Users (The invites will be sent to\
+                                             the emails on save)'},
         ),
         migrations.AddField(
             model_name='reglink',
             name='reminder',
-            field=models.ForeignKey(blank=True, editable=False, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='reglinks', to='gsoc.Scheduler'),
+            field=models.ForeignKey(blank=True, editable=False, null=True,
+                                    on_delete=django.db.models.deletion.CASCADE,
+                                    related_name='reglinks', to='gsoc.Scheduler'),
         ),
         migrations.AlterField(
             model_name='scheduler',
             name='command',
-            field=models.CharField(choices=[('send_email', 'send_email'), ('send_irc_msg', 'send_irc_msg'), ('deactivate_user', 'deactivate_user'), ('send_reg_reminder', 'send_reg_reminder')], max_length=20),
+            field=models.CharField(choices=[('send_email', 'send_email'),
+                                            ('send_irc_msg', 'send_irc_msg'),
+                                            ('deactivate_user', 'deactivate_user'),
+                                            ('send_reg_reminder', 'send_reg_reminder')],
+                                   max_length=20),
         ),
     ]
