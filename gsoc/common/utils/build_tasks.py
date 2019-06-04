0dbb2ba29a65faf85ac57e582d11fcfa29e07b9f
Sounak Pradhan <sounak.98@gmail.com>
2019-06-04 19:27:01 +0530

    Fix typo

--- gsoc/common/utils/build_tasks.py
+++ gsoc/common/utils/build_tasks.py
@@ -52,7 +52,7 @@ def build_post_blog_reminders(builder):
             suborg_admins = UserProfile.objects.filter(suborg_full_name=suborg, role=1)
             student_email = profile.user.email
 
-            mentors_email = ['gsoc-admins@python.org']
+            mentors_emails = ['gsoc-admins@python.org']
             mentors_emails.extend([_.user.email for _ in mentors])
             mentors_emails.extend([_.user.email for _ in suborg_admins])
 
