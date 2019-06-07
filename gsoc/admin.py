1902528314908762f39db40ca6dded7ac015ba82
Sounak Pradhan <sounak.98@gmail.com>
2019-06-07 15:46:53 +0530

    Add event admin

--- gsoc/admin.py
+++ gsoc/admin.py
@@ -405,3 +405,22 @@ class ArticleReviewAdmin(admin.ModelAdmin):
 
 
 admin.site.register(ArticleReview, ArticleReviewAdmin)
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'start_date', 'end_date', 'cal_link')
+
+    def cal_link(self, obj):
+        if obj.link:
+            return mark_safe('<a href="{}" target="_blank">Goto Event</a>'.format(obj.link))
+        else:
+            return None
+    
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
+admin.site.register(Event, EventAdmin)
