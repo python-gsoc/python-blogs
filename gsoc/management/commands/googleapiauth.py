32fd87bb350bf93fa807e27989002719bf03e525
Sounak Pradhan <sounak.98@gmail.com>
2019-06-08 11:13:26 +0530

    Add base_dir to path

--- gsoc/management/commands/googleapiauth.py
+++ gsoc/management/commands/googleapiauth.py
@@ -6,7 +6,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 
 from django.core.management import BaseCommand
 
-from gsoc.settings import GOOGLE_API_SCOPES, GOOGLE_API_CLIENT_CONFIG
+from gsoc.settings import GOOGLE_API_SCOPES, GOOGLE_API_CLIENT_CONFIG, BASE_DIR
 
 
 class Command(BaseCommand):
@@ -14,13 +14,13 @@ class Command(BaseCommand):
     requires_system_checks = False   # for debugging
 
     def handle(self, *args, **options):
-        if os.path.exists('google_api_token.pickle'):
-            os.remove('google_api_token.pickle')
+        if os.path.exists(os.path.join(BASE_DIR, 'google_api_token.pickle')):
+            os.remove(os.path.join(BASE_DIR, 'google_api_token.pickle'))
         
         flow = InstalledAppFlow.from_client_config(
             GOOGLE_API_CLIENT_CONFIG, GOOGLE_API_SCOPES
         )
         creds = flow.run_console()
 
-        with open('google_api_token.pickle', 'wb') as token:
+        with open(os.path.join(BASE_DIR, 'google_api_token.pickle'), 'wb') as token:
             pickle.dump(creds, token)
