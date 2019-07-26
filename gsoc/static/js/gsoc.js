653e9e11e3e92c7617fe0da47e1d70b77aeb3274
Sounak Pradhan <sounak.98@gmail.com>
2019-07-26 13:43:42 +0530

    Combine js and css

--- gsoc/static/js/gsoc.js
+++
@@ -1,9 +0,0 @@
-//js for register button
-$(function() {
-  var chk = $('#check');
-  var btn = $('#btncheck');
-
-  chk.on('change', function() {
-    btn.prop("disabled", !this.checked);//true: disabled, false: enabled
-  }).trigger('change'); //page load trigger event
-});
