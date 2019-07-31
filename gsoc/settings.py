b847a8b11ece55e3605bd933901f0a124e7321a7
Sounak Pradhan <sounak.98@gmail.com>
2019-07-31 10:22:33 +0530

    Add email log handler

--- gsoc/settings.py
+++ gsoc/settings.py
@@ -296,7 +296,8 @@ LOGGING = {
             },
         'mail_admins': {
             'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
+            'class': 'django.utils.log.AdminEmailHandler',
+            'include_html': True
             },
         },
     'loggers': {
