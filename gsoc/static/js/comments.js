bbb6736ab22f6dc0d926951fcb6f63a65217b495
botanicvelious <cyberempires@gmail.com>
2020-05-27 17:04:26 -0600

    Update comments.js

--- gsoc/static/js/comments.js
+++ gsoc/static/js/comments.js
@@ -1,6 +1,7 @@
+/*jshint esversion: 6 */
 const sleep = (milliseconds) => {
-    return new Promise(resolve => setTimeout(resolve, milliseconds))
-}
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+};
 
 function showCommentForm(commentPk) {
     var form = document.getElementById('form-' + commentPk);
@@ -9,7 +10,7 @@ function showCommentForm(commentPk) {
 }
 
 function copyCommentUrl(commentPk) {
-    var href = window.location.href.split('#')[0]
+    var href = window.location.href.split('#')[0];
     var str = href + '#comment-' + commentPk;
     var el = document.createElement('textarea');
     el.value = str;
@@ -21,10 +22,10 @@ function copyCommentUrl(commentPk) {
     document.body.removeChild(el);
 
     var share = document.getElementById(`share-${commentPk}`);
-    share.innerHTML = "<span>Link copied!</span>"
+    share.innerHTML = "<span>Link copied!</span>";
     sleep(3000).then(() => {
         share.innerHTML = `<span onclick="copyCommentUrl(${commentPk})" id="share-${commentPk}">Share</span>`;
-    })
+    });
 }
 
 function deleteComment(commentPk) {
@@ -33,7 +34,7 @@ function deleteComment(commentPk) {
         del.innerHTML = 'Confirm Delete?';
         sleep(3000).then(() => {
             del.innerHTML = 'Delete';
-        })
+        });
     }
     else if (del.innerHTML == 'Confirm Delete?') {
         document.getElementById(`delete-form-${commentPk}`).submit();
@@ -41,11 +42,11 @@ function deleteComment(commentPk) {
 }
 
 (function markSelected() {
-    var parts = window.location.href.split('#')
+    var parts = window.location.href.split('#');
     if (parts.length == 2 && parts[1].split('-')[0] == 'comment') {
         var comment = document.getElementById(parts[1]);
         if (comment) {
-            comment.classList.add('selected')
+            comment.classList.add('selected');
         }
     }
 }());
@@ -53,7 +54,7 @@ function deleteComment(commentPk) {
 function updateCharCount(formId) {
     var textAreaEl = document.getElementById(`comment-textarea-${formId}`);
     var remainingCharEl = document.getElementById(`remaining-chars-${formId}`);
-    len = textAreaEl.value.length;
+    var len = textAreaEl.value.length;
     var remaining = 1000 - len;
     remainingCharEl.innerHTML = `${remaining} characters left`;
 }
