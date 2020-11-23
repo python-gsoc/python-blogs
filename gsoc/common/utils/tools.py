3ca070a5989e6998623c52f6c989c7daacf1e586
botanicvelious <cyberempires@gmail.com>
2020-11-23 00:55:45 -0700

    Update tools.py
    404 fix

--- gsoc/common/utils/tools.py
+++ gsoc/common/utils/tools.py
@@ -73,10 +73,10 @@ def push_site_template(file_path, content):
 def push_images(file_path, content):
     g = Github(settings.GITHUB_ACCESS_TOKEN)
     repo = g.get_repo(settings.STATIC_SITE_REPO)
-    f = repo.get_contents(file_path)
-    if f.sha is not None: 
+    try:
+        f = repo.get_contents(file_path)
         repo.update_file(file_path, f"Add {file_path} logo", content, f.sha)
-    else:
+    except Exception as e:
         repo.create_file(file_path, f"Add {file_path} logo", content)
 
 
