80a2706cbe6aece6e359abb6b4666282f6ccf8c4
botanicvelious <cyberempires@gmail.com>
2022-01-21 13:53:22 -0700

    nuke migrations due to fk issues

---
+++ gsoc/wsgi_windows.py
@@ -0,0 +1,20 @@
+activate_this = 'C:/Users/myuser/Envs/my_application/Scripts/activate_this.py'
+# execfile(activate_this, dict(__file__=activate_this))
+exec(open(activate_this).read(),dict(__file__=activate_this))
+
+import os
+import sys
+import site
+
+# Add the site-packages of the chosen virtualenv to work with
+site.addsitedir('C:\Python39\Lib\site-packages')
+
+# Add the app's directory to the PYTHONPATH
+sys.path.append('C:\Users\Matthew Lagoe\Documents\GitHub\python-blogs')
+sys.path.append('C:\Users\Matthew Lagoe\Documents\GitHub\python-blogs\gsoc')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'gsoc.settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gsoc.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
