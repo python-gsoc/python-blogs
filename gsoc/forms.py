b0eb7f24716315663457cc6ddb9b0610682118a7
Diwash <diwashdahal75@gmail.com>
2022-06-04 09:19:19 +0545

    handle duplicate email when inviting through adduserlog

--- gsoc/forms.py
+++ gsoc/forms.py
@@ -46,6 +46,17 @@ class RegLinkForm(forms.ModelForm):
     class Meta:
         model = RegLink
         fields = ("email", "user_role", "user_suborg", "gsoc_year")
+    
+    def save(self, commit=True):
+        data = super(RegLinkForm, self).save(commit=False)
+        email = self.cleaned_data['email']
+        try:
+            RegLink.objects.get(email=email).delete()
+        except:
+            pass
+        if commit:
+            data.save()
+        return data
 
 
 class BlogPostDueDateForm(forms.ModelForm):
