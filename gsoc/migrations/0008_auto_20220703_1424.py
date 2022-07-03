dcd938f3f178244e1faa27611a5008a76d40b72e
Apache <apache@blogs.python-gsoc.org>
2022-07-03 07:53:38 -0700

    fix migration

--- gsoc/migrations/0008_auto_20220703_1424.py
+++ gsoc/migrations/0008_auto_20220703_1424.py
@@ -42,12 +42,6 @@ class Migration(migrations.Migration):
             field=models.CharField(choices=[('build_pre_blog_reminders', 'build_pre_blog_reminders'), ('build_post_blog_reminders', 'build_post_blog_reminders'), ('build_revoke_student_perms', 'build_revoke_student_perms'), ('build_remove_user_details', 'build_remove_user_details'), ('build_add_timeline_to_calendar', 'build_add_timeline_to_calendar'), ('build_add_bpdd_to_calendar', 'build_add_bpdd_to_calendar'), ('build_add_event_to_calendar', 'build_add_event_to_calendar')], max_length=40),
         ),
         migrations.AlterField(
-            model_name='builder',
-            name='timeline',
-            field=models.ForeignKey(default='', on_delete=django.db.models.deletion.CASCADE, to='gsoc.timeline'),
-            preserve_default=False,
-        ),
-        migrations.AlterField(
             model_name='gsocyear',
             name='gsoc_year',
             field=models.IntegerField(primary_key=True, serialize=False, validators=[gsoc.models.validate_date]),
