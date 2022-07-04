3dce4006f228339fb41ea77e77cd8d66f791f138
Diwash <diwashdahal75@gmail.com>
2022-07-04 13:57:55 +0545

    custom command googleauth

---
+++ gsoc/common/utils/googleauth.py
@@ -0,0 +1,27 @@
+import os
+
+from gsoc.settings import BASE_DIR
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+
+if os.path.exists(os.path.join(BASE_DIR, 'token.json')):
+    creds = Credentials.from_authorized_user_file(
+        os.path.join(BASE_DIR, 'token.json'),
+        SCOPES
+    )
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            os.path.join(BASE_DIR, 'credentials.json'),
+            SCOPES
+        )
+        creds = flow.run_local_server(port=0, open_browser=False)
+    with open(os.path.join(BASE_DIR, 'token.json'), 'w') as token:
+        token.write(creds.to_json())
