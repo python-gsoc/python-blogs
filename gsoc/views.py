88e571445c08ef7e940de9e02f0f6230234ad3f4
Diwash <diwashdahal75@gmail.com>
2022-07-07 20:42:39 +0545

    refresh OAuth token

--- gsoc/views.py
+++ gsoc/views.py
@@ -1,5 +1,6 @@
 import csv
 from datetime import datetime
+
 from gsoc import settings
 
 from .forms import AcceptanceForm, ChangeInfoForm, ProposalUploadForm
@@ -44,6 +45,8 @@ from pdfminer.pdfpage import PDFPage
 from profanityfilter import ProfanityFilter
 
 import google_auth_oauthlib.flow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
 
 
 ROLES = {1: 'Admin', 2: 'Mentor', 3: 'Student'}
@@ -606,21 +609,32 @@ CLIENT_SECRETS_FILE = os.path.join(settings.BASE_DIR, 'credentials.json')
 @decorators.login_required
 @decorators.user_passes_test(is_superuser)
 def authorize(request):
-    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-        CLIENT_SECRETS_FILE,
-        scopes=SCOPES
-    )
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+            with open(os.path.join(settings.BASE_DIR, 'token.json'), 'w') as token:
+                token.write(creds.to_json())
+            return HttpResponse("Token refreshed successfully.")
+        else:
+            flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+                CLIENT_SECRETS_FILE,
+                scopes=SCOPES
+            )
 
-    flow.redirect_uri = settings.OAUTH_REDIRECT_URI + "oauth2callback"
+            flow.redirect_uri = settings.OAUTH_REDIRECT_URI + "oauth2callback"
 
-    authorization_url, state = flow.authorization_url(
-        access_type='offline',
-        include_granted_scopes='true'
-    )
+            authorization_url, state = flow.authorization_url(
+                access_type='offline',
+                include_granted_scopes='true'
+            )
 
-    request.session['state'] = state
+            request.session['state'] = state
 
-    return redirect(authorization_url)
+            return redirect(authorization_url)
+    else:
+        return HttpResponse("Token is valid.")
 
 
 @decorators.login_required
