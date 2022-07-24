c078e85c8a7b6160e7b9bb292c25e694f1920c9b
Diwash <diwashdahal75@gmail.com>
2022-07-24 13:37:07 +0545

    handle error if no DaysConf in Database

--- gsoc/models.py
+++ gsoc/models.py
@@ -208,24 +208,24 @@ class DaysConf(models.Model):
 
 
 # days for reminder
-# try:
-PRE_BLOG_REMINDER = DaysConf.objects.get(title="PRE_BLOG_REMINDER")
-POST_BLOG_REMINDER_FIRST = DaysConf.objects.get(title="POST_BLOG_REMINDER_FIRST")
-POST_BLOG_REMINDER_SECOND = DaysConf.objects.get(title="POST_BLOG_REMINDER_SECOND")
+try:
+    PRE_BLOG_REMINDER = DaysConf.objects.get(title="PRE_BLOG_REMINDER")
+    POST_BLOG_REMINDER_FIRST = DaysConf.objects.get(title="POST_BLOG_REMINDER_FIRST")
+    POST_BLOG_REMINDER_SECOND = DaysConf.objects.get(title="POST_BLOG_REMINDER_SECOND")
 
-BLOG_POST_DUE_REMINDER = DaysConf.objects.get(title="BLOG_POST_DUE_REMINDER")
-UPDATE_BLOG_COUNTER = DaysConf.objects.get(title="UPDATE_BLOG_COUNTER")
+    BLOG_POST_DUE_REMINDER = DaysConf.objects.get(title="BLOG_POST_DUE_REMINDER")
+    UPDATE_BLOG_COUNTER = DaysConf.objects.get(title="UPDATE_BLOG_COUNTER")
 
-REGLINK_REMINDER = DaysConf.objects.get(title="REGLINK_REMINDER")
-# except Exception:
-#     PRE_BLOG_REMINDER = -3
-#     POST_BLOG_REMINDER_FIRST = -1
-#     POST_BLOG_REMINDER_SECOND = 3
+    REGLINK_REMINDER = DaysConf.objects.get(title="REGLINK_REMINDER")
+except Exception:
+    PRE_BLOG_REMINDER = DaysConf(days=-3)
+    POST_BLOG_REMINDER_FIRST = DaysConf(days=1)
+    POST_BLOG_REMINDER_SECOND = DaysConf(days=3)
 
-#     BLOG_POST_DUE_REMINDER = -6
-#     UPDATE_BLOG_COUNTER = 6
+    BLOG_POST_DUE_REMINDER = DaysConf(days=-6)
+    UPDATE_BLOG_COUNTER = DaysConf(days=6)
 
-#     REGLINK_REMINDER = 3
+    REGLINK_REMINDER = DaysConf(days=-3)
 
 
 class SubOrg(models.Model):
